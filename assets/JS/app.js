e828e67b8ef360a831ab43f43377925dada19059
Parameshwari2004 <116253214+Parameshwari2004@users.noreply.github.com>
2023-03-09 18:20:43 +0530

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,42 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+document.getElementById('length').addEventListener('change', charLength)
+
+
+/*uppercase character*/
+
+let upper_case = document.getElementById(uppercase).value;
+let lower_case = document.getElementById(lowercase).value;
+let numbers = document.getElementById(numbers).value;
+let symbols = document.getElementById(symbols).value;
+
+
+function upper(){
+let character 
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowercase = "abcdefghijklmnopqrstuvwsyz",
+    numbers = 1234567890,
+    symbols = "!@#$%+_)(*&^%"
+
+}
+
+
+
+
+
+
+
+
+
+   
+    
+
+
